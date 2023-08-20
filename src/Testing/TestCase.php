7348e08c11f959317b7051f2e7529355f6ff578c
Jason McCreary <jason@pureconcepts.net>
2023-08-20 15:38:33 -0400

    Recent facades during testing

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -3,7 +3,14 @@
 namespace Shift\Cli\Sdk\Testing;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Shift\Cli\Sdk\Facades\Facade;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Facade::clearResolvedInstances();
+    }
 }
