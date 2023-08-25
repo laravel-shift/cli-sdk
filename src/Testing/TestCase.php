c82341c8502fca468b1b254ac2943046e02c686e
Jason McCreary <jason@pureconcepts.net>
2023-08-25 15:14:33 -0400

    Add custom assertion

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -13,4 +13,20 @@ abstract class TestCase extends BaseTestCase
 
         Facade::clearResolvedInstances();
     }
+
+    protected function assertCommentMatching(array $comments, string $content = null, array $paths = null, string $reference = null): void
+    {
+        if (is_null($content) && is_null($paths) && is_null($reference)) {
+            throw new \InvalidArgumentException('At least one of `content`, `paths`, or `reference` arguments must be provided.');
+        }
+
+        $matches = collect($comments)
+            ->where(function (\Shift\Cli\Sdk\Models\Comment $comment) use ($reference, $paths, $content) {
+                return (is_null($content) || str_contains($comment->content(), $content))
+                    && (is_null($paths) || empty(array_diff($comment->paths(), $paths)))
+                    && (is_null($reference) || $reference === $comment->reference());
+            });
+
+        $this->assertCount(1, $matches, 'Failed to find a comment matching the given criteria.');
+    }
 }
